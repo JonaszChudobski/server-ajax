f2fc4889b06bf521fd0cea3803788b28e99bf524
JonaszChudobski <jonasz.chudobski@wp.pl>
2022-11-04 15:15:40 +0100

    server is running

---
+++ public/server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const app = express();
+
+app.use("/public", express.static("public"));
+
+app.get("/public", (req, res) => {
+  res.send("200");
+});
+
+app.listen(3000, () => console.log("Example app is listening on port 3000."));
