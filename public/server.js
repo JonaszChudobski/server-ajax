e9f3b25ac9eb6750f51b298e7405b0f873f16f4c
JonaszChudobski <jonasz.chudobski@wp.pl>
2022-11-09 18:36:37 +0100

    json file returned

--- public/server.js
+++ public/server.js
@@ -6,5 +6,8 @@ app.use("/public", express.static("public"));
 app.get("/public", (req, res) => {
   res.send("200");
 });
+app.get("/public/text.json", (req, res) => {
+  res.send("200");
+});
 
 app.listen(3000, () => console.log("Example app is listening on port 3000."));
