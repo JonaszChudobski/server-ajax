1152180f2f63e2498f33e727acc89cecdd6a06ed
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-01-06 14:19:26 +0100

    methods in a separate file with require

--- public/server.js
+++ public/server.js
@@ -1,7 +1,6 @@
+const methods = require("../src/methods");
 const express = require("express");
 const app = express();
-let data = [];
-let idCounter = 1;
 
 app.use(express.static("../src"));
 app.get("/", function (req, res) {
@@ -9,45 +8,11 @@ app.get("/", function (req, res) {
 });
 
 app.use(express.json());
-app.post("/", (req, res) => {
-  data.push({ userId: idCounter, user: req.body });
-  idCounter += 1;
-  res.status(201).send(data);
-});
+app.post("/", methods.postData);
 
-app.put("/data/:user", (req, res) => {
-  const user = req.params.user;
-  const account = data.map((element) => {
-    element.userId === user;
-  });
-  const userIndex = data
-    .map((element) => {
-      return element.userId;
-    })
-    .indexOf(Number(user));
-  if (!account) {
-    return res.status(404).json({ error: "User does not exist" });
-  }
-  data[userIndex]['user'] = req.body;
-  res.status(201).send(data);
-});
+app.put("/data/:user", methods.putData);
 
-app.delete("/data/:user", (req, res) => {
-  const user = req.params.user;
-  const account = data.map((element) => {
-    element.userId === user;
-  });
-  const userIndex = data
-    .map((element) => {
-      return element.userId;
-    })
-    .indexOf(Number(user));
-  if (!account) {
-    return res.status(404).json({ error: "User does not exist" });
-  }
-  data.splice(userIndex, 1);
-  res.send(JSON.stringify(data));
-});
+app.delete("/data/:user", methods.deleteData);
 
 const server = app.listen(3000, function () {
   const host = "127.0.0.1";
--- src/methods.js
+++ src/methods.js
@@ -1,10 +1,15 @@
-export const postData = (req, res) => {
+let data = [];
+let idCounter = 1;
+const express = require("../public/node_modules/express");
+const app = express();
+
+module.exports.postData = (req, res) => {
   data.push({ userId: idCounter, user: req.body });
   idCounter += 1;
   res.status(201).send(data);
 };
 
-export const putData = (req, res) => {
+module.exports.putData = (req, res) => {
   const user = req.params.user;
   const account = data.map((element) => {
     element.userId === user;
@@ -17,11 +22,11 @@ export const putData = (req, res) => {
   if (!account) {
     return res.status(404).json({ error: "User does not exist" });
   }
-  data[userIndex]['user'] = req.body;
+  data[userIndex]["user"] = req.body;
   res.status(201).send(data);
 };
 
-export const deleteData = (req, res) => {
+module.exports.deleteData = (req, res) => {
   const user = req.params.user;
   const account = data.map((element) => {
     element.userId === user;
