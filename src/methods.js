47b98ea78eb273601cd2fc0020bc63242fc13747
JonaszChudobski <jonasz.chudobski@wp.pl>
2023-01-20 14:37:20 +0100

    data are in a Map structure with UUID identifier

--- src/methods.js
+++ src/methods.js
@@ -1,44 +1,21 @@
-let data = [];
-let idCounter = 1;
-const express = require("../public/node_modules/express");
-const app = express();
+const crypto = require("crypto");
 
-module.exports.postData = (req, res) => {
-  data.push({ userId: idCounter, user: req.body });
-  idCounter += 1;
-  res.status(201).send(data);
+module.exports.createUuid = () => {
+  return (String(1e7) + -1e3 + -4e3 + -8e3 + -1e11).replace(/[018]/g, (c) =>
+    (Number(c) ^ (crypto.randomBytes(1)[0] & (15 >> (Number(c) / 4)))).toString(
+      16
+    )
+  );
 };
 
-module.exports.putData = (req, res) => {
-  const user = req.params.user;
-  const account = data.map((element) => {
-    element.userId === user;
-  });
-  const userIndex = data
-    .map((element) => {
-      return element.userId;
-    })
-    .indexOf(Number(user));
-  if (!account) {
-    return res.status(404).json({ error: "User does not exist" });
-  }
-  data[userIndex]["user"] = req.body;
-  res.status(201).send(data);
+module.exports.findElement = (data, user) => {
+  account = data.get(user);
 };
 
-module.exports.deleteData = (req, res) => {
-  const user = req.params.user;
-  const account = data.map((element) => {
-    element.userId === user;
-  });
-  const userIndex = data
-    .map((element) => {
-      return element.userId;
-    })
-    .indexOf(Number(user));
-  if (!account) {
-    return res.status(404).json({ error: "User does not exist" });
-  }
-  data.splice(userIndex, 1);
-  res.send(JSON.stringify(data));
+module.exports.putData = (data, user, body) => {
+  data.set(user, body);
+};
+
+module.exports.deleteData = (data, user) => {
+  data.delete(user);
 };
